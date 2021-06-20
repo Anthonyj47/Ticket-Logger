7e2c1343ac4a0f05226f6c9eec987f38b0ace381
Anthony <anthonyj8147@gmail.com>
2021-06-20 14:27:27 +0200

    Commiting for push

--- JS/main.js
+++ JS/main.js
@@ -2,6 +2,13 @@ let vm = new Vue({
     el: '#app',
     data: {
         count: "0",
+        //input values
+        ticket_num: undefined,
+        call_driver: undefined,
+        store: undefined,
+        action: undefined,
+        add_action: undefined,
+        outcome: undefined,
     },
     computed: {},
     methods: {}
